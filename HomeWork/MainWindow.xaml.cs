a6d6596c76ac33203c57e661dbf59d18ff8c78e0
Джабашев Нурбек Кадербекович <@corp.itstep>
2019-05-20 21:13:28 +0600

    DocumentViewer

---
+++ HomeWork/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Xps.Packaging;
+
+namespace HomeWork
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        
+        }
+
+        private void Print_Click(object sender, RoutedEventArgs e)
+        {
+            PrintDialog pd = new PrintDialog();
+            if (pd.ShowDialog() == true)
+            {
+                pd.PrintVisual(targetPrint, "Текстовый элемент и кнопка");
+            }
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
